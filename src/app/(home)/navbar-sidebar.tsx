3c5df7cda5bf4fbc01271878c5daf8d2ebf49e37
jefferson.brandao <jefferson.brandao@g.globo>
2025-04-15 23:02:43 -0300

    criando estilos da home

---
+++ src/app/(home)/navbar-sidebar.tsx
@@ -0,0 +1,65 @@
+import { ScrollArea } from "@/components/ui/scroll-area";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import Link from "next/link";
+
+interface NavbarItemProps {
+  href: string;
+  children: React.ReactNode;
+}
+
+interface Props {
+  items: NavbarItemProps[];
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+const NavbarSidebar = ({ items, open, onOpenChange }: Props) => {
+  return (
+    <Sheet open={open} onOpenChange={onOpenChange}>
+      <SheetContent side="left" className="p-0 transition-none">
+        <SheetHeader className="p-4 border-b">
+          <div className="flex items-center">
+            <SheetTitle>Menu</SheetTitle>
+          </div>
+        </SheetHeader>
+
+        <ScrollArea className="flex flex-col overflow-y-auto h-full pb-2">
+          {items.map((item) => (
+            <Link
+              key={item.href}
+              href={item.href}
+              className="w-full text-left p-4 hover:bg-black hover:text-white flex items-center text-base font-medium"
+              onClick={() => onOpenChange(false)}
+            >
+              {item.children}
+            </Link>
+          ))}
+
+          <div className="border-t">
+            <Link
+              href={"/entrar"}
+              className="w-full text-left p-4 hover:bg-black hover:text-white flex items-center text-base font-medium"
+              onClick={() => onOpenChange(false)}
+            >
+              Entrar
+            </Link>
+            <Link
+              href={"/comecar-a-vender"}
+              className="w-full text-left p-4 hover:bg-black hover:text-white flex items-center text-base font-medium"
+              onClick={() => onOpenChange(false)}
+            >
+              Começar a vender
+            </Link>
+          </div>
+        </ScrollArea>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default NavbarSidebar;
